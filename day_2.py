796669d7da97dc1964e29906522ed0449ecc8b6c
ckkashi <ckkashi007@gmail.com>
2022-09-12 14:49:47 +0500

    day 2 added

---
+++ day_2.py
@@ -0,0 +1,49 @@
+# name = "hunain"
+# age = 19
+# a = "this is {0}, and age is {1}".format(name,age)
+# print(a)
+
+# a = "this is {name}, and age is {age}".format(name = "Hunain",age = 12)
+# print(a)
+
+# dictionaries
+# dict = {"fname":"hunain","lname":"surahio"}
+# print(dict)
+# print(len(dict))
+# dict["fname"] = "ABC"
+# dict.update({"lname":"xyz"})
+# dict["age"] = 12
+# print(dict.get("fname"))
+# print(dict.keys())
+# print(dict.values())
+# print(dict.items())
+# for key,value in dict.items():
+#     print(key," : ",value)
+
+# tuple
+# a = ("Hello","world")
+# print(a)
+# b = list(a)
+# print(b)
+# b.append("2001")
+# b = tuple(b);
+# a = b;
+# print(a)
+# print(type(a))
+
+# loops
+# while loop
+# tab_no = 2 ;
+# i = 1;
+# while i <= 10:
+#     print(tab_no," x ", i," = ",i*tab_no)
+#     i+=1
+
+#for loop
+# tab_no = 2 
+# for i in range(1,11):
+#     print(tab_no," x ", i," = ",i*tab_no)
+
+# for i in range(2,22,2):
+#     print(i)
+
